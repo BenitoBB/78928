95913d995c44b1618db8c462e13e3f021d90f51a
Benito <zs22016070@estudiantes.uv.mx>
2025-03-07 10:37:28 -0600

    Rutas en Flask01

--- flask01/app.py
+++ flask01/app.py
@@ -2,10 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') #lo vuelve aplicacion web
+@app.route('/saludar') #lo vuelve aplicacion web
 def hola_mundo():
     return 'Hola Mundo!'
 
+@app.route('/despedir') #otro tipo de ruta o "directorio"
+def adios_mundo():
+    return 'Adios Mundo!'
+
+@app.route('/hola') 
+def hola_html():
+    return '<h1 style="color:red">HOLA</h1>'
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"benito"}'
+
+@app.route('/xml')
+def xml():
+    return '<?xml version="1.0"?>  <nombre>Benito</nombre>'
+
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',
-debug=True)
+    app.run(host = '0.0.0.0', debug=True)
